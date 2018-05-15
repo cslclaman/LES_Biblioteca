53791834f90470f608897d8e2cf39ad4f25a6c6a
Caique Siqueira <cslclaman@gmail.com>
2018-05-15 20:00:32 -0300

    Adiciona classe Pessoa

---
+++ Biblioteca_BackEnd/model/pessoa.model.js
@@ -0,0 +1,21 @@
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema;
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+var pessoaSchema=new Schema({
+
+    _idPessoa: Number,
+    nome: { type: String, required: true },
+    dataNascimento: { type: Date, required: true },
+    cpf: { type: String, required: true },
+    endereco: { type: String, required: false },
+    telefone: { type: String, required: false },
+    email: { type: String, required: false },
+    login: { type: String, required: true },
+    senha: { type: String, required: true }
+    
+});
+
+pessoaSchema.plugin(AutoIncrement, {inc_field: '_idPessoa'});
+
+module.exports=mongoose.model('Pessoa',pessoaSchema);
