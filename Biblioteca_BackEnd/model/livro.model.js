58f7f3b0bcdfe3c1989832c7aed5309647c113ba
Caique Siqueira <cslclaman@gmail.com>
2018-06-12 20:31:18 -0300

    Altera parâmetro (edição não é mais obrigatório)

--- Biblioteca_BackEnd/model/livro.model.js
+++ Biblioteca_BackEnd/model/livro.model.js
@@ -10,7 +10,7 @@ var livroSchema=new Schema({
         { type: mongoose.Schema.Types.ObjectId, ref: 'Autor' }
     ],
     editora: { type: String, required: true },
-    edicao: { type: Number, required: true },
+    edicao: { type: Number, required: false },
     genero: { type: String, required: false },
     numPaginas: { type: Number, required: false },
     ano: { type: Number, required: false },
