84fd8c58f100875878cdf73658c27a4e45142e55
Caique Siqueira <cslclaman@gmail.com>
2018-06-22 17:39:12 -0300

    Adiciona funcionalidade de Login e mensagem de erro

--- BibliotecaFrontEnd/src/app/users/login/login.component.ts
+++ BibliotecaFrontEnd/src/app/users/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { user } from 'app/user';
 import { Http } from '@angular/http';
 import { Headers } from '@angular/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,11 +14,13 @@ export class LoginComponent implements OnInit {
   username: string;
   password: string;
   http: Http;
+  router: Router;
   userlogin: user;
+  message: string;
 
-  constructor(http: Http) {
-
+  constructor(http: Http, router: Router) {
     this.http = http;
+    this.router = router;
   }
 
   ngOnInit() {
@@ -25,6 +28,7 @@ export class LoginComponent implements OnInit {
 
   login(event) {
     event.preventDefault();
+    this.message = "";
 
     this.userlogin = new user(this.username, this.password);
     let hdr = new Headers();
@@ -34,9 +38,16 @@ export class LoginComponent implements OnInit {
     .post('http://localhost:3000/api/login', JSON.stringify(this.userlogin),{ headers: hdr})
     .subscribe(res => {
         let resultado = res.json();
-        console.log(resultado);
+        if (resultado.status == "OK"){
+          //Armazena dados do usuário no armazenamento local do computador
+          localStorage.setItem('usuario', JSON.stringify(resultado.usuario));          
+          //Redireciona para o menu
+          this.router.navigate(['/menu']);
+        }else{
+          //Apresenta mensagem de erro na tela
+          this.message = resultado.message;
+          return;
+        }
     });
-    console.log(this.username);
-    console.log(this.password);
   }
 }
